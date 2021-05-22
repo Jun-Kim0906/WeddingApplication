88926cf93091b676292a5a373a3d19bca29ed1a8
yhrie99 <user.name>
2021-05-22 17:29:41 +0900

    [UPDATE] Edit HomePage Appar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text('Home'),
       ),
       body: Center(
         child: Column(
